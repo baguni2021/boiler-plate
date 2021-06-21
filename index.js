21bee882ae6546391e307f780ddb2fcf9828f0cd
forid48@gmail.com <forid48@gmail.com>
2021-06-21 13:20:48 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key.js');
+const { User } = require('./models/User.js');
+
+console.log(config.mongoURI) 
+//application/x-www-gorm-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+//application/json
+app.use(express.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://esnature:1q1q1Q1Q@cluster0.q0wp2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false // 에러방지 구문
 }). then(()=> console.log('MongoDB connected...'))
 .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요')
+})
+
+app.post('/register', (req, res) => {
+  //회원가입할때 필요한 정보를 client  에서 가져오면 
+  //그것들을 데이터베이스에 넣는다
+
+  const user = new User(req.body)
+
+  user.save((err, userinfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
