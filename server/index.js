283beaabd697883cc2bbddd32500ae314c4a4f67
forid48@gmail.com <forid48@gmail.com>
2021-06-25 05:11:18 +0900

    27강 끝내고...

---
+++ server/index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key.js');
+const { auth } = require('./middleware/auth');
+const { User } = require('./models/User.js');
+
+//application/x-www-gorm-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+//application/json
+app.use(express.json());
+app.use(cookieParser());
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false // 에러방지 구문
+}). then(()=> console.log('MongoDB connected...'))
+.catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {res.send('Hello World! 안녕하세요')})
+
+app.get('/api/hello', (req, res) => {res.send("안녕하세요~")})
+
+app.post('/api/users/register', (req, res) => {
+  //회원가입할때 필요한 정보를 client  에서 가져오면 
+  //그것들을 데이터베이스에 넣는다
+
+  const user = new User(req.body)
+
+  user.save((err, userinfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+app.post('/api/users/login', (req, res) => {
+  console.log('1-S');
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당되는 유저가 없습니다."
+      })
+    }
+    console.log('1-E');
+
+  // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      console.log('2-true인지 false인지 확인');
+      if(!isMatch)
+      return res.json({ loginSuccess: false, message:"비밀번호가 틀렸습니다."})
+
+      // 비밀번호까지 맞다면 토큰을 생성하기
+      user.generateToken((err, user) => {
+        console.log('3-ok');
+        if(err) return res.status(400).send(err);
+
+        //토큰을 저장한다, 어디에? 쿠키, 로컬스토로지...
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id })
+      })
+    })
+  })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+
+  // 여기까지  미들웨어를 통과해 왔다는 얘기는 Authentication이 true라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true, 
+      // role 0이 아니면 : 어드민, role 0이면  일반유저
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lasrname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({_id: req.user._id },
+    { token: ""}, (err, user) => {
+      if(err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
+
+const port = 5000
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
