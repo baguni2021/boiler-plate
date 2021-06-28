e020c37ccbd548deb5f7efb28317c356d00abb47
forid48@gmail.com <forid48@gmail.com>
2021-06-28 14:01:04 +0900

    완강하며...

--- client/src/index.js
+++ client/src/index.js
@@ -3,26 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { provider } from 'react-redux';
-
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { applymiddleware, createStore } from 'redux';
-import { prmiseMiddleware } from 'redux-promise';
-import { ReduxThunk } from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers'
-
-const createStateWithMiddleware = applymiddleware(prmiseMiddleware, ReduxThunk) (createStore)
-
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 ReactDOM.render(
-  <provider store = {createStateWithMiddleware(Reducer,
+  <Provider store = {createStoreWithMiddleware(Reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && 
     window.__REDUX_DEVTOOLS_EXTENSION__()
-  )}>
+  )}
+  >
     <App />
-  </provider>
+  </Provider>
   , document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
